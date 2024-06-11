962c066fdf963d9b812dfc3b2306e3462ae96b44
mahedee <mahedee.hasan@gmail.com>
2024-06-10 21:41:42 -0400

    Update initial project setup

--- backend/PlanCards/PlanCards.API/Program.cs
+++ backend/PlanCards/PlanCards.API/Program.cs
@@ -1,36 +1,6 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-namespace PlanCards.API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+app.MapGet("/", () => "Hello World!");
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
-}
+app.Run();
