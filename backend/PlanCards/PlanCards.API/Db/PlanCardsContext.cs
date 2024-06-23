de0eb2a749dbebbcdb33213ef486738637545411
mahedee <mahedee.hasan@gmail.com>
2024-06-23 14:26:08 -0400

    add db context

---
+++ backend/PlanCards/PlanCards.API/Db/PlanCardsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlanCards.API.Models;
+
+namespace PlanCards.API.Db
+{
+    public class PlanCardsContext : DbContext
+    {
+        public PlanCardsContext(DbContextOptions<PlanCardsContext> options) : base(options)
+        {
+        }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
--- backend/PlanCards/PlanCards.API/Program.cs
+++ backend/PlanCards/PlanCards.API/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PlanCards.API.Db;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+// Add services to the container.
+builder.Services.AddDbContext<PlanCardsContext>(options =>
+        options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
